483ee6003dcdd98213a3a364afda7cec3b61e1c4
zzNatezz <thc.dzch@gmail.com>
2024-03-10 14:17:45 +0700

    finish test useEffect

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -4,35 +4,29 @@ import React, { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
-export const Contexts = ({children}) => {
-    const [listPendingSong, setPendingSong] = useState([]);
-    const [loading, setLoading] = useState(false)
+export const Contexts = ({ children }) => {
+    const [pendingSongs, setPendingSongs] = useState([]);
 
-    const isPending = async () =>{
-        
-        // const song =  await axios.get('https://be-song.vercel.app/v1/songs')
-        // const allSong = song.data.allSong;
-        // const pendingSong = allSong.filter(item => item.status === 'pending')
-        console.log(`pendingSong`);
-    }
-
-    // useEffect( ()=> {
-    //     async function fetchData (){
-    //     const getAPI = axios.get('https://be-song.vercel.app/v1/songs');
-    //     const allSong = getAPI.data.allSong;
-    //     const pendingSong = allSong.filter(item => item.status === 'pending')
-    //     setPendingSong(pendingSong);
-    //     console.log(pendingSong);
-    // }
-
-    // },[])
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const getData = await axios.get('https://be-song.vercel.app/v1/songs');
+                const allSong = getData.data.allSong.filter((song) => song.status === 'pending');
+                setPendingSongs(allSong);
+                console.log(allSong);
+            } 
+            catch (error) 
+            {
+                console.log(`Something went wrong`, error)
+            }
+        }; fetchData();
+    }, []);
 
     return (
         <AppContext.Provider
-        value={{
-            isPending,
-            listPendingSong,
-        }}
+            value={{
+                pendingSongs,
+            }}
         >
             {children}
         </AppContext.Provider>
--- src/layout/Section/welcome/Notification/Notice.jsx
+++ src/layout/Section/welcome/Notification/Notice.jsx
@@ -1,24 +1,24 @@
 import React, { useContext } from "react";
 import "./Notice.scss";
-import SongDB from "../../../../localStorage/SongDB";
 import { AppContext } from "../../../../Context/Context";
 
+
 function Notice() {
-  const {isPending, listPendingSong} = useContext(AppContext)
+  const { pendingSongs,} = useContext(AppContext)
   return (
     <div className="notice-main-container">
-      {listPendingSong.map((item, index) => (
+      {pendingSongs.map((item, index) => (
         <div className="notice-container" key={index}>
           <div className="notice-content">
-            <h2 className="notice-title">Song Name : {item.title}</h2>
+            <h2 className="notice-title">Song : {item.title}</h2>
             <h3 className="notice-author">Artist : {item.author}</h3>
-            <img className="notice-img" src={item.imagine} alt="img" />
+            <img className="notice-img" src={item.image.url} alt="img" />
             <audio className="notice-audio" controls>
-              <source src={item.song} type="audio/mpeg" />
+              <source src={item.song.url} type="audio/mpeg" />
             </audio>
           </div>
           <div className="notice-button">
-            <button onClick={ () => isPending()}> Approve </button>
+            <button> Approve </button>
             <button> Cancle </button>
           </div>
         </div>
